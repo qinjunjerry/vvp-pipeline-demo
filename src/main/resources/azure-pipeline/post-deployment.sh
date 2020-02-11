6beadd4b413d5e7010a3a56491f05f68d8b608a4
Jun Qin <11677043+qinjunjerry@users.noreply.github.com>
2020-02-11 17:27:32 +0100

    extract commitHash in script, set-local-access.sh

--- src/main/resources/azure-pipeline/post-deployment.sh
+++ src/main/resources/azure-pipeline/post-deployment.sh
@@ -1,3 +1,9 @@
+# extract commit hash
+# We use the commit hash in jar URI when POST/PATCH a deployment in VVP, e.g.,
+#     wasbs://...../job.jar?commit=e81c66d
+# This way VVP will suspend & start the flink job upon a PATCH action
+commitHash=`echo ${buildVersion} | cut -c 1-7`
+
 curl -X POST "http://localhost:8080/api/v1/namespaces/${vvpNamespace}/deployments" \
     -H "Authorization: Bearer ${vvpAPIToken}" \
     -H "accept: application/yaml" -H "Content-Type: application/yaml" -s -d "
