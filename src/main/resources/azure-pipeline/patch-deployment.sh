bc1d297d77eb3939809bc9868b4028d830ca1abe
Jun Qin <11677043+qinjunjerry@users.noreply.github.com>
2020-02-11 13:59:39 +0100

    change to bach env variables

--- src/main/resources/azure-pipeline/patch-deployment.sh
+++ src/main/resources/azure-pipeline/patch-deployment.sh
@@ -1,14 +1,16 @@
-echo "PATCH Deployment $(deploymentId)"
-curl -X PATCH "http://localhost:8080/api/v1/namespaces/$(vvpNamespace)/deployments/$(deploymentId)" \
--H "Authorization: Bearer $(vvp-pipeline-demo-api-token)" \
--H "accept: application/yaml" -H "Content-Type: application/yaml" -s -d '
-kind: Deployment
-apiVersion: v1
-spec:
-  state: RUNNING
-  template:
+#!/usr/bin/env bash
+
+echo "PATCH Deployment ${deploymentId}"
+curl -X PATCH "http://localhost:8080/api/v1/namespaces/${vvpNamespace}/deployments/${deploymentId}" \
+    -H "Authorization: Bearer ${vvp-pipeline-demo-api-token}" \
+    -H "accept: application/yaml" -H "Content-Type: application/yaml" -s -d "
+    kind: Deployment
+    apiVersion: v1
     spec:
-      artifact:
-        jarUri: >-
-          wasbs://$(blobContainer)@$(storageAccount).blob.core.windows.net/artifacts/namespaces/$(vvpNamespace)/$(artifactName)?commit=$(commitHash)
-'
+      state: RUNNING
+      template:
+        spec:
+          artifact:
+            jarUri: >-
+              wasbs://${blobContainer}@${storageAccount}.blob.core.windows.net/artifacts/namespaces/${vvpNamespace}/${artifactName}?commit=${commitHash}
+    "
