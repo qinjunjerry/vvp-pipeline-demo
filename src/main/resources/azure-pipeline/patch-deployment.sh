57030fc3c7a7be33245dc5013d4ef7a18c968183
Jun Qin <11677043+qinjunjerry@users.noreply.github.com>
2020-02-11 14:49:21 +0100

    vvpAPIToken

--- src/main/resources/azure-pipeline/patch-deployment.sh
+++ src/main/resources/azure-pipeline/patch-deployment.sh
@@ -1,13 +1,13 @@
 echo deploymentId: ${deploymentId}
 echo vvpNamespace: ${vvpNamespace}
-echo vvp-pipeline-demo-api-token: ${vvp-pipeline-demo-api-token}
+echo vvpAPIToken: ${vvpAPIToken}
 echo blobContainer: ${blobContainer}
 echo artifactName: ${artifactName}
 echo storageAccount: ${storageAccount}
 echo commitHash: ${commitHash}
 
 echo curl -X PATCH "http://localhost:8080/api/v1/namespaces/${vvpNamespace}/deployments/${deploymentId}" \
-    -H "Authorization: Bearer ${vvp-pipeline-demo-api-token}" \
+    -H "Authorization: Bearer ${vvpAPIToken}" \
     -H "accept: application/yaml" -H "Content-Type: application/yaml" -s -d "
     kind: Deployment
     apiVersion: v1
@@ -21,7 +21,7 @@ echo curl -X PATCH "http://localhost:8080/api/v1/namespaces/${vvpNamespace}/depl
     "
 
 curl -X PATCH "http://localhost:8080/api/v1/namespaces/${vvpNamespace}/deployments/${deploymentId}" \
-    -H "Authorization: Bearer ${vvp-pipeline-demo-api-token}" \
+    -H "Authorization: Bearer ${vvpAPIToken}" \
     -H "accept: application/yaml" -H "Content-Type: application/yaml" -s -d "
     kind: Deployment
     apiVersion: v1
